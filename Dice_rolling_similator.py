03afc5b5b9824dd1725f6db1f668b173244ebeca
Oennie <76390970+Oennie@users.noreply.github.com>
2020-12-25 14:56:07 +0100

    refactored codes of dice simulator and uncommon characters

--- Dice_rolling_similator.py
+++ Dice_rolling_similator.py
@@ -1,18 +1,23 @@
 # Allow user to roll dice
 # use choice to pick a number
-# allow user to roll again 
+# allow user to roll again till he wants to stop
 
 import random
 
 print("\nWelcome gamer\nLet's roll the dice!")
-
-roll_dice = str(input("Roll the dice? :  "))
 die = [1, 2, 3, 4, 5, 6]
-print(random.choice(die))
+roll_dice = int(input("Roll the dice? \nIf yes type '1', If no type '2' :  "))
 
-roll_again = str(input("Would you like to roll again? \nIf yes type 'continue', If no type 'stop' :  "))
-if roll_again == "stop":
-    print("Thank you playing!")
-else:
-    die = [1, 2, 3, 4, 5, 6]
-    print(random.choice(die))
+if roll_dice != 1:
+        print("Thank you for playing!")
+print("You rolled:" , random.choice(die))
+
+roll_again = int(input("Would you like to roll again? \nIf yes type '1', If no type '2' :  "))
+if roll_again != 1:
+        print("Thank you for playing!")
+while roll_again == 1:
+    print("You rolled:" , random.choice(die))
+    roll_again = int(input("Would you like to roll again? \nIf yes type '1', If no type '2' :  "))  
+    if roll_again != 1:
+        print("Thank you for playing!")
+        break
--- uncommon_characters_concatenation.py
+++ uncommon_characters_concatenation.py
@@ -8,7 +8,18 @@ def solve(str_a, str_b):
     set_a = set(str_a)
     set_b = set(str_b)
     common_character = list(set_a & set_b)
-    result = [ch for ch in str_a if ch not in common_character] + [ch for ch in str_b if ch not in common_character]
+    un_chr1 = []
+    un_chr2 = []
+    for chr_ in str_a:
+        if chr_ not in common_character:
+            un_chr1.append(chr_)
+    for chr_ in str_b:
+        if chr_ not in common_character:     
+            un_chr2.append(chr_)
+    result = (un_chr1 + un_chr2)
+    
+            
+# result = [ch for ch in str_a if ch not in common_character] + [ch for ch in str_b if ch not in common_character]
     print( ''.join(result) )
 
 solve("adbc", "dfga")
